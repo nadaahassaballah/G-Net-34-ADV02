7a084776fddb049d341a270ab24b2abe15ef32df
Nada Hamdy <nadaahasabbalh@gmail.com>
2026-03-25 00:18:48 +0200

    Add project files.

---
+++ G-Net-34-ADV02/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_Net_34_ADV02
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
